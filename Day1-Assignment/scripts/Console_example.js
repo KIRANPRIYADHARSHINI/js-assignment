06b6461416d03ad416d23f2d1b6592fc3bca2a5f
KIRANPRIYADHARSHINI <68219793+KIRANPRIYADHARSHINI@users.noreply.github.com>
2020-07-15 01:59:53 -0700

    Day 1 - Assignment

---
+++ Day1-Assignment/scripts/Console_example.js
@@ -0,0 +1,31 @@
+// 1. Console log() Method
+
+console.log("Hello Javascript")
+console.log("25")
+console.log("Null")
+console.log("True")
+
+// 2. Console error() Method
+
+console.error("An error occurred")
+
+// 3. Console warn() Method
+
+console.warn("This is a Warning Message")
+
+// 4. Console table() Method
+
+console.table({Name : "Kiran", Age : "23", City: "Chennai "})
+console.log('%c Custom Message', 'color:blue;')
+
+// 5. console clear() Method
+
+console.clear()
+
+// 6. console time() and timeEnd() Method
+
+console.time('Time Taken');
+console.log("Hey");
+console.error("an error occurred");
+console.warn("Hey this a warning");
+console.timeEnd('Time Taken');
---
+++ Day1-Assignment/scripts/varletconst.js
@@ -0,0 +1,20 @@
+// Var
+
+var name="kiran" 
+var age=24;
+var canfly=false;
+var languages= ['Tamil','English'];
+
+
+// Let
+{
+    let name="chinnu";
+    name="varsha";
+    console.log("Uisng Let: ",name);
+    console.log(name); 
+}
+
+// Const
+const country = "India";
+console.log(country);
+

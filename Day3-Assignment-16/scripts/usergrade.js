2bf531558f0aace4f084b7375c6d16f8a7507bc9
KIRANPRIYADHARSHINI <68219793+KIRANPRIYADHARSHINI@users.noreply.github.com>
2020-07-17 01:28:06 -0700

    Day 3 - Assignment

---
+++ Day3-Assignment-16/scripts/usergrade.js
@@ -0,0 +1,53 @@
+// Conditional 
+let grade = prompt("Enter your Mark" , "<Marks goes here>");
+
+        if (grade >= 80)
+        {
+            console.log("Marks ares " + grade + "and grade is A");
+        } 
+        else if (grade >= 50) 
+        {
+            console.log("Marks ares " + grade + "and grade is B");
+        } 
+        else if (grade >= 30) 
+        {
+            console.log("Marks ares " + grade + "and grade is C");
+        } 
+        
+        else {
+            console.log("You have not secured any Grade");
+        }
+
+// let grade1 = prompt("Enter your Marks")
+
+// switch(grade1 >= 50)
+// {
+//     case 1:
+//         console.log("Marks ares " + grade1 + "and grade is B");
+//         break;
+
+//     default:
+//         console.log("You secured below 50 marks");
+// }
+
+// function markgrade()
+// {
+//     grade3=document.form1.text1.value;
+//     switch (grade)
+//     {   
+//     case 'A':
+//     console.log("Marks ares " + grade3 + "and grade is A");
+//     break;
+
+//     case 'B':
+//     console.log("Marks ares " + grade3 + "and grade is B");
+//     break;
+
+//     case 'C':
+//     console.log("Marks ares " + grade2 + "and grade is C");
+//     break;
+//     default:
+//     console.log("Wrong grade.........");
+//     }
+    
+// }
